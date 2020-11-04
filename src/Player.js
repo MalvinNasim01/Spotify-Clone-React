2626d98922044ab9af474e74a403089cf02e5e01
Malvin Nasim <malvinnasim007@gmail.com>
2020-11-04 23:17:04 +0500

    Initial Commit

---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Player.css";
+
+function Player() {
+  return (
+    <div className="player">
+      <h1>Welcome to SPOTIFY!!</h1>
+    </div>
+  );
+}
+
+export default Player;
